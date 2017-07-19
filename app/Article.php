680400259d925cfafa66df4af846ce32f80b400e
cricrespo <cricrespo@gmail.com>
2017-07-19 14:18:07 +0200

    Creating Relations into tables with php artisan tinker

--- app/Article.php
+++ app/Article.php
@@ -4,8 +4,33 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Article extends Model
-{
+class Article extends Model {
+
     protected $table = "articles";
     protected $fillabe = ['title', 'content', 'category_id', 'user_id'];
+
+    public function category(){
+
+        return $this->belongsTo('App\Category');
+
+    }
+
+    public function user(){
+
+        return $this->belongsTo('App\User');
+
+    }
+
+    public function images(){
+
+        return $this->hasMany('App\Image');
+
+    }
+
+
+    public function tags(){
+
+        return $this->belongsToMany('App\Tag');
+    }
+
 }
--- app/Tag.php
+++ app/Tag.php
@@ -11,4 +11,9 @@ class Tag extends Model
 
     protected $fillable = ['name'];
 
+    public function articles(){
+
+        return $this->belongsToMany('App\Article')->withTimestamps();
+    }
+
 }

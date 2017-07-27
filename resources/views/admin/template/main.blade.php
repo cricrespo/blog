72578ea496e4e0a209969de42c7bdc2690ee4024
cricrespo <cricrespo@gmail.com>
2017-07-27 14:49:47 +0200

    Use of Blade with, @includes, @yield. implemented main page with section

---
+++ resources/views/admin/template/main.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', 'Default') | Admin </title>
+    <link rel="stylesheet" href="{{ asset('plugin/bootstrap/css/bootstrap.css') }}">
+
+    <script src="{{ asset('plugin/jquery/js/jquery-3.2.1.js') }}"></script>
+    <script src="{{ asset('plugin/bootstrap/js/bootstrap.js') }}"></script>
+
+</head>
+<body>
+    {{-- Incluimos el nav de navegación   --}}
+    @include('admin.template.partials.nav')
+    
+    {{-- Incluimos mediante el yield, el contenido de welcome --}}
+    <section class="">
+        @yield('content')
+    </section>   
+
+
+</html>

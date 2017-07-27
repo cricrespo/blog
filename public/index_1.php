85c34d19ea69972c3c7759bce78da4f3027b70a9
cricrespo <cricrespo@gmail.com>
2017-07-27 09:56:31 +0200

    configure routes to show views and add routes with ::group

---
+++ public/index_1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Blog</title>
+    <link rel="stylesheet" href="css/app.css">
+  </head>
+  <body>
+    <h2>Welcome Blog</h2>
+  </body>
+</html>
---
+++ resources/views/articles.blade.php
@@ -0,0 +1,3 @@
+<html>
+    <h1>Articles</h1>
+</html>
